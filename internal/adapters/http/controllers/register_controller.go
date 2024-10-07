98149fb5cc979cda6ac0f5219084d56f1e02ec9d
Lucas Lima <lukkasoliveiralima@gmail.com>
2024-10-07 16:05:13 -0300

    init register controller

--- internal/adapters/http/controllers/register_controller.go
+++ internal/adapters/http/controllers/register_controller.go
@@ -1,7 +1,50 @@
 package controllers
 
-import "net/http"
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/go-playground/validator/v10"
+	"net/http"
+)
 
 func RegisterController(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Register user!"))
+	validate := validator.New()
+	var request UserRequest
+
+	if err := json.NewDecoder(r.Body).Decode(&request); err != nil {
+		http.Error(w, "Invalid request body", http.StatusBadRequest)
+	}
+
+	errors := validate.Struct(request)
+	if errors != nil {
+		var errorResponse []ValidationErrorResponse
+		for _, err := range errors.(validator.ValidationErrors) {
+			message := fmt.Sprintf("Field validation for '%s' failed on the '%s' tag", err.Field(), err.Tag())
+			errorResponse = append(errorResponse, ValidationErrorResponse{
+				err.Field(),
+				message,
+			})
+		}
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusBadRequest) // 400 Bad Request
+		json.NewEncoder(w).Encode(map[string]interface{}{
+			"errors": errorResponse,
+		})
+		return
+	}
+
+	fmt.Println(request)
+
+	w.Write([]byte("User created"))
+}
+
+type UserRequest struct {
+	Name     string `json:"name" validate:"required,min=2,max=100"`
+	Email    string `json:"email" validate:"required,email"`
+	Password string `json:"password" validate:"required,min=8"`
+}
+
+type ValidationErrorResponse struct {
+	Field   string `json:"field"`
+	Message string `json:"message"`
 }
