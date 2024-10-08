571db654039df0745f82df1a087823a32b2ebb80
Lucas Lima <lukkasoliveiralima@gmail.com>
2024-10-08 20:22:41 -0300

    implement controller logic

--- internal/adapters/http/controllers/register_controller.go
+++ internal/adapters/http/controllers/register_controller.go
@@ -1,8 +1,13 @@
 package controllers
 
 import (
-	"fmt"
+	"encoding/json"
+	"github.com/lukkas-lukkas/golang-todo-list-api/internal/adapters/db"
+	encrypt2 "github.com/lukkas-lukkas/golang-todo-list-api/internal/adapters/encrypt"
 	"github.com/lukkas-lukkas/golang-todo-list-api/internal/adapters/http/requests"
+	"github.com/lukkas-lukkas/golang-todo-list-api/internal/adapters/http/responses"
+	"github.com/lukkas-lukkas/golang-todo-list-api/internal/usecases/register_user"
+	"log"
 	"net/http"
 )
 
@@ -10,11 +15,30 @@ func RegisterController(w http.ResponseWriter, r *http.Request) {
 
 	var request requests.RegisterRequest
 
+	json.NewDecoder(r.Body).Decode(&request)
+
 	if validated := requests.Validated(request, w); !validated {
 		return
 	}
 
-	fmt.Println(request)
+	repo := db.NewUserRepository(db.DB)
+	encrypt := encrypt2.NewBcryptService()
+	useCase := register_user.NewRegisterUserService(repo, encrypt)
 
-	w.Write([]byte("User created"))
+	user, err := useCase.CreateUser(register_user.UserDto{
+		Name:     request.Name,
+		Email:    request.Email,
+		Password: request.Password,
+	})
+	if err != nil {
+		log.Println("Error registering user", err)
+		http.Error(w, "Internal server error", http.StatusBadGateway)
+		return
+	}
+
+	json.NewEncoder(w).Encode(responses.RegisterUserResponse{
+		ID:    user.ID,
+		Name:  user.Name,
+		Email: user.Email,
+	})
 }
