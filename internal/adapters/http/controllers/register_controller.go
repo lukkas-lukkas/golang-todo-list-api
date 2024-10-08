c998f654114450ea5e28a2e682e7f73260f126f3
Lucas Lima <lukkasoliveiralima@gmail.com>
2024-10-08 17:30:10 -0300

    add request validation

--- internal/adapters/http/controllers/register_controller.go
+++ internal/adapters/http/controllers/register_controller.go
@@ -1,50 +1,20 @@
 package controllers
 
 import (
-	"encoding/json"
 	"fmt"
-	"github.com/go-playground/validator/v10"
+	"github.com/lukkas-lukkas/golang-todo-list-api/internal/adapters/http/requests"
 	"net/http"
 )
 
 func RegisterController(w http.ResponseWriter, r *http.Request) {
-	validate := validator.New()
-	var request UserRequest
 
-	if err := json.NewDecoder(r.Body).Decode(&request); err != nil {
-		http.Error(w, "Invalid request body", http.StatusBadRequest)
-	}
+	var request requests.RegisterRequest
 
-	errors := validate.Struct(request)
-	if errors != nil {
-		var errorResponse []ValidationErrorResponse
-		for _, err := range errors.(validator.ValidationErrors) {
-			message := fmt.Sprintf("Field validation for '%s' failed on the '%s' tag", err.Field(), err.Tag())
-			errorResponse = append(errorResponse, ValidationErrorResponse{
-				err.Field(),
-				message,
-			})
-		}
-		w.Header().Set("Content-Type", "application/json")
-		w.WriteHeader(http.StatusBadRequest) // 400 Bad Request
-		json.NewEncoder(w).Encode(map[string]interface{}{
-			"errors": errorResponse,
-		})
+	if validated := requests.Validated(request, w); !validated {
 		return
 	}
 
 	fmt.Println(request)
 
 	w.Write([]byte("User created"))
-}
-
-type UserRequest struct {
-	Name     string `json:"name" validate:"required,min=2,max=100"`
-	Email    string `json:"email" validate:"required,email"`
-	Password string `json:"password" validate:"required,min=8"`
-}
-
-type ValidationErrorResponse struct {
-	Field   string `json:"field"`
-	Message string `json:"message"`
 }
--- internal/config/router.go
+++ internal/config/router.go
@@ -9,6 +9,7 @@ import (
 func CreateRouter() chi.Router {
 	r := chi.NewRouter()
 	r.Use(middleware.Logger)
+	r.Use(middleware.AllowContentType("application/json"))
 
 	r.Get("/", controllers.HelloWorldController)
 	r.Post("/register", controllers.RegisterController)
